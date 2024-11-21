cbb8bd41c96afdb04b6bd4bb3580ce018c38b6f3
Nic Gordon <hello@gor.do>
2024-11-21 12:39:29 +1100

    Update web route

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,32 @@
 
 use BradyRenting\FilamentPasswordless\Http\Controllers\HandleMagicLinkController;
 use Illuminate\Support\Facades\Route;
+use Filament\Facades\Filament;
 
-Route::domain(config('filament.domain'))
-    ->middleware(config('filament.middleware.base'))
-    ->prefix(config('filament.path'))
-    ->name('filament.auth.login.')
-    ->group(function () {
-        // Magic link route...
-        Route::get('/login/magic-link/{key}/{remember?}', HandleMagicLinkController::class)
-            ->middleware('signed')
-            ->name('magic-link');
-    });
+foreach (Filament::getPanels() as $panel) {
+    if (! $panel->hasPlugin('filament-passwordless')) {
+        continue;
+    }
+
+    $path = $panel->getPath();
+    $loginSlug = $panel->getLoginRouteSlug();
+
+    $domains = $panel->getDomains();
+
+    foreach ((empty($domains) ? [null] : $domains) as $domain) {
+        Filament::currentDomain($domain);
+
+        Route::domain($domain)
+            ->middleware($panel->getMiddleware())
+            ->prefix($path)
+            ->name('filament.auth.login.')
+            ->group(function () use ($loginSlug) {
+                // Magic link route
+                Route::get("$loginSlug/magic-link/{key}/{remember?}", HandleMagicLinkController::class)
+                    ->middleware('signed')
+                    ->name('magic-link');
+            });
+
+        Filament::currentDomain(null);
+    }
+}
